e64b2bc41875335f2dacda2ad8d0360cc4cb46e0
Manuel Mendez <mmendez534@gmail.com>
2020-07-30 17:28:08 -0400

    previous

--- pkgs/applications/terminal-emulators/kitty/default.nix
+++ pkgs/applications/terminal-emulators/kitty/default.nix
@@ -21,14 +21,14 @@
 with python3Packages;
 buildPythonApplication rec {
   pname = "kitty";
-  version = "0.19.3";
+  version = "unstable-2021-04-07g${builtins.substring 0 9 src.rev}";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "kovidgoyal";
     repo = "kitty";
-    rev = "v${version}";
-    sha256 = "0r49bybqy6c0n1lz6yc85py80wb40w757m60f5rszjf200wnyl6s";
+    rev = "4822fa989e890958de6c988cc9dd597ef54b7331";
+    sha256 = "1mzmam8hz0imfxzavad4c5wn7whkkhp55qx3xlmn4zldgd3d0804";
   };
 
   buildInputs = [

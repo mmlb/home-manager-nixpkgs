2a7fb87fc8eabd72ec1b0e163d1e13c11abf70b6
Manuel Mendez <mmendez534@gmail.com>
2020-07-30 17:28:08 -0400

    previous

--- pkgs/shells/elvish/default.nix
+++ pkgs/shells/elvish/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "elvish";
-  version = "unstable-2021-02-02g${builtins.substring 0 9 src.rev}";
+  version = "unstable-2021-08-06g${builtins.substring 0 9 src.rev}";
 
   excludedPackages = [ "website" ];
 
@@ -11,11 +11,11 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "elves";
     repo = pname;
-    rev = "786d61260db18b786176a24e6e34963f29acb213";
-    sha256 = "1whzvylnsql81pz34lmd8fv3nxp31263pb56v1nw6bgj11df5b2w";
+    rev = "6741619169a6a54b282b016bd8e77fec8754f6b7";
+    sha256 = "1i8rxsdx32vzn42i8d7sgig1aw7nmrng0811vsxczyxfmz0kxynb";
   };
 
-  vendorSha256 = "1nn5sj04dkx457s5pjlhpnmxpgdy3zzq5l7vgjkynpmikfwvlvxp";
+  vendorSha256 = "0cmsijnvkz6p6kz2gymlxshjzgbdwp6agwpzlpzd4bvzp8d71mp6";
   CGO_ENABLED = 0;
 
   doCheck = false;

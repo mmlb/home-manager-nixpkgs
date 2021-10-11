585a4f5760f4be45e1f1b18672fd21ee9f39ab46
Manuel Mendez <mmendez534@gmail.com>
2021-10-11 12:22:39 -0400

    previous

--- pkgs/applications/editors/kakoune/default.nix
+++ pkgs/applications/editors/kakoune/default.nix
@@ -5,12 +5,12 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "kakoune-unwrapped";
-  version = "unstable-2020-09-01g${builtins.substring 0 9 src.rev}";
+  version = "unstable-2022-07-19g${builtins.substring 0 9 src.rev}";
   src = fetchFromGitHub {
     owner = "mawww";
     repo = "kakoune";
-    rev = "e0d2602e6a924c9a4067fa9ff23f034b906dd56d";
-    sha256 = "091qzk0qs7hql0q51hix99srgma35mhdnjfd5ncfba1bmc1h8x5i";
+    rev = "559af669c77a3b3b6de0376f177ebbba5ebd0328";
+    sha256 = "172k3cb9fdk5bb8wypxgm73xlni45xmwhzlsfl3k2fi2133x98q4";
   };
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ ncurses asciidoc docbook_xsl libxslt ];

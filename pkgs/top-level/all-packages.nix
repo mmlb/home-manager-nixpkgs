f79f3411f1b1d931f07026ede2100350417d8540
Manuel Mendez <mmendez534@gmail.com>
2022-03-29 12:44:49 -0400

    evlish: build with go1.18

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1825,7 +1825,7 @@ with pkgs;
 
   sedutil = callPackage ../tools/security/sedutil { };
 
-  elvish = callPackage ../shells/elvish { };
+  elvish = callPackage ../shells/elvish { buildGoModule = buildGo118Module; };
 
   emplace = callPackage ../tools/package-management/emplace { };
 
